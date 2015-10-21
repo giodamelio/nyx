dbd0bb33bbb5597333c954f0f15112f3e9300e59
Gio d'Amelio <giodamelio@gmail.com>
2015-10-21 16:41:25 -0700

    React-Bootstrap hello world

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router';
+import {
+  Navbar,
+  NavBrand,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem,
+} from 'react-bootstrap';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar inverse toggleNavKey={0}>
+          <NavBrand>Nyx</NavBrand>
+          <Nav right eventKey={0}> {/* This is the eventKey referenced */}
+            <NavItem eventKey={1} href="#">Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="collapsible-navbar-dropdown">
+              <MenuItem eventKey="1">Action</MenuItem>
+              <MenuItem eventKey="2">Another action</MenuItem>
+              <MenuItem eventKey="3">Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey="4">Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
+
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, Link } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 import domready from 'domready';
 
+import App from './app';
 import Index from './pages/index';
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-        </ul>
-
-        {this.props.children}
-      </div>
-    );
-  }
-}
 
 domready(function() {
   ReactDOM.render(
